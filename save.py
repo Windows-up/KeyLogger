280d4256618b278fb2460f943de363fa41631be4
Doctor <jebpip2008@gmail.com>
2021-10-10 21:14:54 +0300

    [1.0] good version FIX SPECAL BUTTONS

--- save.py
+++ save.py
@@ -1,8 +1,11 @@
 
 def writeData(text):
-    _file = open("users.txt", "r")
-    _read = _file.read()
-    _file.close()
+    try:
+        _file = open("users.txt", "r")
+        _read = _file.read()
+        _file.close()
+    except:
+        _read = ""
     _file = open("users.txt", "w")
     _file.write(_read + text)
     _file.close()
--- translators.py
+++ translators.py
@@ -3,7 +3,13 @@ _specal_words = {
     "enter" : "\n",
     "decimal" : ".",
 
-    "delete" : "",
+    "delete" : " [DEL]",
+    "backspace": " [BACK]",
+    "right": " {RIGHT}",
+    "left": " {LEFT}",
+    "up": " {UP}",
+    "down": " {DOWN}",
+
     "insert" : "",
     "home" : "",
     "end": "",
@@ -11,7 +17,22 @@ _specal_words = {
     "page down" : "",
     "num lock" : "",
     "ctrl" : "",
-    "shift" : ""
+    "shift" : "",
+    "esc" : "",
+    "tab" : "",
+
+    "f1" : " (F1)",
+    "f2" : " (F2)",
+    "f3" : " (F3)",
+    "f4" : " (F4)",
+    "f5" : " (F5)",
+    "f6" : " (F6)",
+    "f7" : " (F7)",
+    "f8" : " (F8)",
+    "f9" : " (F9)",
+    "f10" : " (F10)",
+    "f11" : " (F11)",
+    "f12" : " (F12)",
 }
 
 
